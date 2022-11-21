7c6ff53ea92f0a9eca4f58727f97cc5ac764d4a1
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2022-11-21 09:33:19 +0300

    Created Campaigns component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Campaigns from "../components/Campaings";
 import Carousel from "../components/carousel";
 
 const Index = () => {
   return (
-    <div className="relative">
+    <div className="">
       <Carousel />
+      <Campaigns />
     </div>
   );
 };
