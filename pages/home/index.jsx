117fd9df1906c4440e3bcd58912efe304d7a5427
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2022-11-21 09:43:35 +0300

    Created MenuItem and MenuWrapper components.

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Campaigns from "../components/Campaings";
 import Carousel from "../components/carousel";
+import MenuWrapper from "../components/product/MenuWrapper";
 
 const Index = () => {
   return (
     <div className="">
       <Carousel />
       <Campaigns />
+      <MenuWrapper />
     </div>
   );
 };
